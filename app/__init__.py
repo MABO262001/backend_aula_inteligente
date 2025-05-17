aa88ad6709965926366db4fa9ac2d67ceb9a83c5
MABO262001 <ballivian.miguel@ficct.uagrm.edu.bo>
2025-05-17 18:56:11 -0400

    cambios en la bd

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,47 @@
-from flask import Flask, send_from_directory
+# app/__init__.py
+from flask import Flask
 from .config import Config
 from .extensions import db, migrate
 from .routes.user_routes import user_bp
-from .models import *  # importa todos los modelos
 
-import os
+# ⚠️ Importar los modelos explícitamente para que se registren en SQLAlchemy
+# 1 No depende De Nadie
+from .models.user import User
+from .models.profesor import Profesor
+from .models.materia import Materia
+from .models.dia import Dia
+from .models.horario import Horario
+from .models.curso import Curso
+from .models.paralelo import Paralelo
+from .models.gestion import Gestion
+from .models.subgestion import Subgestion
+from .models.apoderado import Apoderado
+from .models.estudiante import Estudiante
+
+# 2 Depende de Otros Modelos
+from .models.dia_horario import DiaHorario
+from .models.materia_profesor import MateriaProfesor
+from .models.curso_paralelo import CursoParalelo
+from .models.parentesco import Parentesco
+
+# 3 Depende del #2
+from .models.matricula import Matricula
+from .models.gestion_curso_paralelo import GestionCursoParalelo
+from .models.materia_profesor_dia_horario import MateriaProfesorDiaHorario
+
+# 4 Depende del #3
+from .models.boleta_inscripcion import BoletaInscripcion
+from .models.materia_horario_curso_paralelo import MateriaHorarioCursoParalelo
+
+# 5 Depende del #4
+from .models.nota import Nota
+from .models.asistencia import Asistencia
+from .models.participacion import Participacion
+
+# 6 Depende del #5
+from .models.estudiante_asistencia import EstudianteAsistencia
+from .models.estudiante_participa import EstudianteParticipa
+
 
 def create_app():
     app = Flask(__name__)
